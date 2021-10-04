3554088b1652f72da29c342f118b96d1b2ab2bb6
utdmn2 <utdmn2@gmail.com>
2021-10-04 07:39:17 +0000

    [Add]booksコントローラー

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'about' => 'homes#about'
+  resource :books
 end
