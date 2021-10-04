0db07d4f6916c235530451ffa9e5b787a9834118
utdmn2 <utdmn2@gmail.com>
2021-10-04 23:12:11 +0000

    [Add]バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,33 +1,49 @@
 class BooksController < ApplicationController
 
-  def new
+  def index
+    @user = current_user
     @book = Book.new
+    @books = Book.all
+
   end
 
   def create
     @book = Book.new(book_params)
+    @books = Book.all
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    @user = @book.user
+    if @book.save
+    flash[:notice] = "You have created book successfully."
+    redirect_to books_path(@book)
+    else
+    render :index
+    end
   end
 
-  def index
-    @books = Book.all
-  end
+
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @booknew = Book.new
   end
 
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book.id)
+    else
+    render :edit
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'about' => 'homes#about'
-  resources :books
+  resources :books, only: [:index, :show, :edit, :update, :destroy, :create]
   resources :users, only: [:show, :index, :edit, :update]
 end
